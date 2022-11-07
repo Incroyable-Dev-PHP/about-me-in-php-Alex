da7f385f4b1bc9eecb2805165655318a622b0bd8
AlexGrimpeur <moreaualexandre2002@gmail.com>
2022-11-07 13:58:38 +0100

    first commit

---
+++ php/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <!-- head included here -->
+    <!-- ... -->
+
+    <body>
+        <div class="container">
+            <?php
+                // your code here with conditions
+                // ...
+            ?>
+        </div>
+    </body>
+
+</html>
